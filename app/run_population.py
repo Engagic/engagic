0eb72b498b0e47fadd63198e826726b037116fbf
iban <ibansadowski@icloud.com>
2025-07-06 20:03:04 -0400

    okay let's go

--- app/run_population.py
+++ app/run_population.py
@@ -3,17 +3,17 @@ from uszipcode import SearchEngine, SimpleZipcode
 
 with SearchEngine() as search:
     session = search.ses
-
-    results = (
-        session.query(
-            SimpleZipcode.major_city,
-            SimpleZipcode.state,
-            func.sum(SimpleZipcode.population).label("population"),
+    if session is not None:
+        results = (
+            session.query(
+                SimpleZipcode.major_city,
+                SimpleZipcode.state,
+                func.sum(SimpleZipcode.population).label("population"),
+            )
+            .group_by(SimpleZipcode.major_city, SimpleZipcode.state)
+            .order_by(func.sum(SimpleZipcode.population).desc())
+            .all()
         )
-        .group_by(SimpleZipcode.major_city, SimpleZipcode.state)
-        .order_by(func.sum(SimpleZipcode.population).desc())
-        .all()
-    )
 
     for city, state, pop in results:
         print(f"{city}, {state}: {pop}")
