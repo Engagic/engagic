78c4ffcae1bc98c45e6f4251412b80d1215edac2
ibansd <56172193+ibansd@users.noreply.github.com>
2025-11-23 03:35:05 +0000

    meta tags dark mode

--- userland/scripts/weekly_digest.py
+++ userland/scripts/weekly_digest.py
@@ -220,6 +220,8 @@ def build_digest_email(
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="color-scheme" content="light dark">
+    <meta name="supported-color-schemes" content="light dark">
     <title>This week in {city_name}</title>
 {DARK_MODE_CSS}
 </head>
